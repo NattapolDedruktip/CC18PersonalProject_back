e729c10ebeaa6ceadd9e97747350946211aa4660
NattapolDedruktip <dedruktip.n@gmail.com>
2024-10-12 09:46:08 +0700

    map add start

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,95 +1,85 @@
-const prisma = require("../config/prisma")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const createError = require("../utils/createError")
+const prisma = require("../config/prisma");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const createError = require("../utils/createError");
 
+module.exports.register = async (req, res, next) => {
+  try {
+    //1 validate req.body ** joi
+    const { email, password, confirmPassword, role } = req.input;
 
+    //2 is there this email in db ?
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
 
-module.exports.register = async (req,res,next) => {
-    try {
-        //1 validate req.body ** joi
-        const {email,password,confirmPassword,role} = req.input 
-        
-
-        //2 is there this email in db ?
-        const user = await prisma.user.findFirst({
-            where : {
-                email
-            },
-        });
-
-        if(user) {
-            return createError(400,"email is already exist")
-        }
-
-        //3 Encrypt password with bcryptjs
-        const hashedPassword = await bcrypt.hash(password,10);
-        
-
-        //4 register success
-        const newUser = await prisma.user.create({
-            data : {
-                email : email,
-                password : hashedPassword,
-                role : role
-            },
-        });
-        res.json({message : "register successfully"})
-
-    } catch (err) {
-        next(err)
+    if (user) {
+      return createError(400, "email is already exist");
     }
 
-}
+    //3 Encrypt password with bcryptjs
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    //4 register success
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+        role: role,
+      },
+    });
+    res.json({ message: "register successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports.login = async (req,res,next) => {
-    try {
-        //1 vilidate req.body
-        const {email,password} = req.input
+module.exports.login = async (req, res, next) => {
+  try {
+    //1 vilidate req.body
+    const { email, password } = req.input;
 
-        //2 is there user in db ?
-        const user = await prisma.user.findFirst({
-            where : {
-                email
-            }
-        })
+    //2 is there user in db ?
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
 
-        if(!user) {
-            return createError(400,"Email or Password is Invalid")
-        }
-
-        //3 check password match
-
-        const isPasswordMatch = await bcrypt.compare(password,user.password)
-
-        if(!isPasswordMatch) {
-            return createError(400,"Email or Password is Invalid!")
-        }
-
-        //4 create payload
-        const payload = {
-            user : {
-                id : user.id,
-                email : user.email,
-                role : user.role
-            },
-        };
-
-        console.log(payload)
-
-        //5 generate token
-        const genToken = jwt.sign(payload,process.env.SECRET,{ expiresIn : "1d" });
-
-
-        //6 send to front-end
-        res.json({
-            user : payload,
-            token : genToken
-        });
-
-    } catch (err) {
-        console.log(err)
-        next(err)
+    if (!user) {
+      return createError(400, "Email or Password is Invalid");
     }
-}
+
+    //3 check password match
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return createError(400, "Email or Password is Invalid!");
+    }
+
+    //4 create payload
+    const payload = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+      image: user.image,
+    };
+
+    console.log(payload);
+
+    //5 generate token
+    const genToken = jwt.sign(payload, process.env.SECRET, { expiresIn: "1d" });
+
+    //6 send to front-end
+    res.json({
+      payload,
+      token: genToken,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,26 +3,56 @@
 //npx prisma db push
 //npx prisma db seed
 
-const bcrypt = require("bcryptjs")
-const { PrismaClient} = require("@prisma/client")
-const prisma = new PrismaClient()
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const hashedPassword = bcrypt.hashSync("123456",10)
-
+const hashedPassword = bcrypt.hashSync("123456", 10);
 
 //pasword 123456
 const userData = [
-    {firstName : "Andy",lastName : "Codecamp",password : hashedPassword , gender : "MALE", email : "anddy@ggg.mail" , image : "https://www.svgrepo.com/show/446525/avatar.svg" , role : "USER"},
-    {firstName : "Bobby",lastName : "Codecamp",password : hashedPassword , gender : "MALE" , email : "bobby@ggg.mail" ,image : "https://www.svgrepo.com/show/446478/avatar-portrait.svg" , role : "HOST"},
-    {firstName : "Candy",lastName : "Codecamp",password : hashedPassword , gender : "FEMALE" , email : "candy@ggg.mail" , image : "https://www.svgrepo.com/show/446491/avatar.svg", role : "USER"},
-    {firstName : "Danny",lastName : "Codecamp",password : hashedPassword , gender : "ETC" , email : "danny@ggg.mail" , image : "https://www.svgrepo.com/show/446490/avatar-portrait.svg" , role : "HOST"},
-]
+  {
+    firstName: "Andy",
+    lastName: "Codecamp",
+    password: hashedPassword,
+    gender: "MALE",
+    email: "anddy@gmail.com",
+    image: "https://www.svgrepo.com/show/446525/avatar.svg",
+    role: "USER",
+  },
+  {
+    firstName: "Bobby",
+    lastName: "Codecamp",
+    password: hashedPassword,
+    gender: "MALE",
+    email: "bobby@ggg.gmail.com",
+    image: "https://www.svgrepo.com/show/446478/avatar-portrait.svg",
+    role: "HOST",
+  },
+  {
+    firstName: "Candy",
+    lastName: "Codecamp",
+    password: hashedPassword,
+    gender: "FEMALE",
+    email: "candy@gmail.com",
+    image: "https://www.svgrepo.com/show/446491/avatar.svg",
+    role: "USER",
+  },
+  {
+    firstName: "Danny",
+    lastName: "Codecamp",
+    password: hashedPassword,
+    gender: "ETC",
+    email: "danny@gmail.com",
+    image: "https://www.svgrepo.com/show/446490/avatar-portrait.svg",
+    role: "HOST",
+  },
+];
 
-
-console.log("DB seed ...")
+console.log("DB seed ...");
 
 async function run() {
-    await prisma.user.createMany({data: userData})
+  await prisma.user.createMany({ data: userData });
 }
 
-run()
+run();
